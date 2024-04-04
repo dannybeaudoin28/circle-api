d725c214b43be0cf382333366e66cdda123136f2
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2024-04-04 14:53:15 -0400

    Add postUser method to service and controller

--- src/main/java/com/beaudoin/circleapi/controller/UserController.java
+++ src/main/java/com/beaudoin/circleapi/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.beaudoin.circleapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.beaudoin.circleapi.data.model.User;
 import com.beaudoin.circleapi.service.UserService;
 
 @RestController
@@ -10,7 +17,20 @@ import com.beaudoin.circleapi.service.UserService;
 public class UserController {
     private UserService userService;
 
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @RequestMapping(value = "/post-user", method = RequestMethod.POST)
+    public ResponseEntity<Integer> postUser(@RequestBody User user) {
+        boolean responseCode = false;
+
+        if (user != null) {
+            responseCode = userService.postUser(user);
+            if (responseCode)
+                return new ResponseEntity<Integer>(200, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(422, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/beaudoin/circleapi/service/UserService.java
+++ src/main/java/com/beaudoin/circleapi/service/UserService.java
@@ -1,8 +1,31 @@
 package com.beaudoin.circleapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.beaudoin.circleapi.data.model.User;
+import com.beaudoin.circleapi.data.repository.UserRepository;
 
 @Service
 public class UserService {
-    
+
+    private UserRepository userRepo;
+
+    @Autowired
+    public UserService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public boolean postUser(User user) {
+        if (user != null) {
+            try {
+                userRepo.save(user);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
 }
