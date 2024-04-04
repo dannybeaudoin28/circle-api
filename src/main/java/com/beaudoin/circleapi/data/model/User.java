3b5ce9f9c08c2b1fb81050b63cd50befd116a6c4
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2024-04-04 14:04:21 -0400

    Modify User.java class to have all necessary getters setters and constructors

--- src/main/java/com/beaudoin/circleapi/data/model/User.java
+++ src/main/java/com/beaudoin/circleapi/data/model/User.java
@@ -9,6 +9,17 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "USER_TABLE")
 public class User {
+
+    public User() {}
+
+    public User(String userEmail, String userFName, String userLName, char userMiddleInitial, String userPassword) {
+        this.userEmail = userEmail;
+        this.userFName = userFName;
+        this.userLName = userLName;
+        this.userMiddleInitial = userMiddleInitial;
+        this.userPassword = userPassword;
+    }
+
     @Id
     @GeneratedValue
     @Column(name = "USER_ID")
@@ -31,4 +42,60 @@ public class User {
 
     @Column(name = "USER_IMG")
     private byte[] userImage;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserFName() {
+        return userFName;
+    }
+
+    public void setUserFName(String userFName) {
+        this.userFName = userFName;
+    }
+
+    public String getUserLName() {
+        return userLName;
+    }
+
+    public void setUserLName(String userLName) {
+        this.userLName = userLName;
+    }
+
+    public char getUserMiddleInitial() {
+        return userMiddleInitial;
+    }
+
+    public void setUserMiddleInitial(char userMiddleInitial) {
+        this.userMiddleInitial = userMiddleInitial;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public byte[] getUserImage() {
+        return userImage;
+    }
+
+    public void setUserImage(byte[] userImage) {
+        this.userImage = userImage;
+    }
 }
