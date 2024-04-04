bf3d13f76cc264b7951ff1992b348b07bfa96b44
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2024-04-04 19:16:50 -0400

    message controller basic functionality implemented

--- src/main/java/com/beaudoin/circleapi/controller/MessageController.java
+++ src/main/java/com/beaudoin/circleapi/controller/MessageController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,18 @@ public class MessageController {
     @Autowired
     public MessageController(MessageService messageService) { 
         this.messageService = messageService;
+    }
+
+    @RequestMapping(value = "/post-message", method = RequestMethod.POST)
+    public ResponseEntity<Integer> postMessage(@RequestBody Message message) {
+        boolean responseCode = false;
+
+        if (message != null) {
+            responseCode = messageService.saveMessage(message);
+            if (responseCode) 
+                return new ResponseEntity<>(422, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(422, HttpStatus.BAD_REQUEST);
     }
 
     @RequestMapping(value = "/get-messages", method = RequestMethod.GET)
